d974c060cbdf5976d967e1947e72ad62a7999588
Pedro Andrade <pedro@drivedeal.io>
2019-01-11 16:49:28 -0200

    adding atrigger verification file (fixed)

--- routes/index.js
+++ routes/index.js
@@ -19,15 +19,4 @@ router.get('/ping', function(req, res, next) {
   res.send('OK')
 });
 
-router.get('/ATriggerVerify.txt', function(req, res, next) {
-  res.send(```CDBDE21DCC1A2E0047AF582EB616AF72
-
-This is ATrigger.com API Verification File.
-This file should be placed on the root folder of target url. This file is unique for each account in ATrigger.com
-http://example.com/mySite/Task?name=joe        This file should be available at: http://example.com/ATriggerVerify.txt
-http://sub.example.com/mySite/Task?name=joe    This file should be available at: http://sub.example.com/ATriggerVerify.txt
-
-```)
-});
-
 module.exports = router;
